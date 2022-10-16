3ef738c657dbcc4e3d062aef7b4e9a5ca87cc34d
Tayfun Top <tayfun111423@gmail.com>
2022-10-16 16:09:45 +0300

    Position refreshed

--- src/components/spinner/spinners.js
+++ src/components/spinner/spinners.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Spinners() {
   return (
     <Spinner animation="border" role="status" 
-    style={{fontSize: "36px", width: "60px", height: "60px", color:"rgb(144, 196, 215)"}}
+    style={{fontSize: "36px", width: "60px", height: "60px", color:"rgb(144, 196, 215)", "position":"absolute", "top":"50px", right: "80px"}}
     >
       <span className="visually-hidden"></span>
     </Spinner>
--- src/components/toast/toast.js
+++ src/components/toast/toast.js
@@ -7,7 +7,7 @@ function Toasts() {
   const { toastShow, setToastShow } = useWeather();
 
   return (
-    <Toast style={{"position":"absolute", "top":"10px"}} onClose={() => setToastShow(false)} show={toastShow} delay={3000} autohide>
+    <Toast style={{"position":"absolute", "top":"10px", right: "0px"}} onClose={() => setToastShow(false)} show={toastShow} delay={3000} autohide>
       <Toast.Header>
         <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
         <strong className="me-auto">Message</strong>
