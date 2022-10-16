812d204be55ebe0fdd5419b0437195359fc35e6b
Tayfun Top <tayfun111423@gmail.com>
2022-10-16 15:39:38 +0300

    Spinner refreshed location

--- src/components/spinner/spinners.js
+++ src/components/spinner/spinners.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Spinners() {
   return (
     <Spinner animation="border" role="status" 
-    style={{fontSize: "36px", width: "60px", height: "60px", marginLeft: "80px", color:"rgb(144, 196, 215)"}}
+    style={{fontSize: "36px", width: "60px", height: "60px", color:"rgb(144, 196, 215)"}}
     >
       <span className="visually-hidden"></span>
     </Spinner>
